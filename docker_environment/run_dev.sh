4535dc7c4f18343e983ba96e6d0a6fe935aa2f2d
Diego Francisco Valenzuela Iturra <diegovalenzuelaiturra@gmail.com>
2021-11-09 18:29:44 -0300

    fix: created missing folder

--- docker_environment/run_dev.sh
+++ docker_environment/run_dev.sh
@@ -20,10 +20,10 @@ fi
 # We import values from the configuration file.
 source ./config.sh
 
-# For more info, run: docker run --help
-#   --init: Makes process PID=1 be docker-init backed by tini: https://docs.docker.com/engine/reference/run/#specify-an-init-process
 parentDir="$(dirname "$(pwd -P)")"
 
+# For more info, run: docker run --help
+#   --init: Makes process PID=1 be docker-init backed by tini: https://docs.docker.com/engine/reference/run/#specify-an-init-process
 docker run --publish ${hostPort}:${containerPort} \
            --volume $parentDir"/lib/:/home/development/lib" \
            $additionalVolume \
@@ -33,7 +33,9 @@ docker run --publish ${hostPort}:${containerPort} \
            --ipc=host \
            ${imageName}:${versionTag}
 
-# docker exec ${containerName} bash -c 'printf "{\n\t\"python.defaultInterpreterPath\": \"/usr/bin/python3\",\n\t\"python.experiments.enabled\": false,\n\t\"jupyter.experiments.enabled\": false\n}\n" > /root/.vscode-server/data/Machine/settings.json && cat /root/.vscode-server/data/Machine/settings.json'
-docker exec ${containerName} bash -c 'printf "{\n\t\"python.experiments.enabled\": false,\n\t\"jupyter.experiments.enabled\": false\n}\n" > /root/.vscode-server/data/Machine/settings.json && cat /root/.vscode-server/data/Machine/settings.json'
+# Opt out from `vscode` experiments
+# docker exec --tty --interactive ${containerName} bash -c 'mkdir -p /root/.vscode-server/data/Machine/ && printf "{\n\t\"python.defaultInterpreterPath\": \"/usr/bin/python3\",\n\t\"python.experiments.enabled\": false,\n\t\"jupyter.experiments.enabled\": false\n}\n" > /root/.vscode-server/data/Machine/settings.json && cat /root/.vscode-server/data/Machine/settings.json'
+docker exec --tty --interactive ${containerName} bash -c 'mkdir -p /root/.vscode-server/data/Machine/ && printf "{\n\t\"python.experiments.enabled\": false,\n\t\"jupyter.experiments.enabled\": false\n}\n" > /root/.vscode-server/data/Machine/settings.json && cat /root/.vscode-server/data/Machine/settings.json'
 
+# Install `teselagen` library in "editable" mode in the container
 docker exec ${containerName} bash -c "cd development/lib; python3 setup.py develop"
