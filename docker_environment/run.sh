f47daf281296e0883bbb1d55c29b2911c7b8d371
Diego Francisco Valenzuela Iturra <diegovalenzuelaiturra@gmail.com>
2021-11-09 13:15:54 -0300

    added `--ipc=host` to `docker run` command

--- docker_environment/run.sh
+++ docker_environment/run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+
 # We import values from the configuration file.
 source ./config.sh
 
-# https://docs.docker.com/config/containers/container-networking/
-
 # For more info, run : docker run --help
-# --publish list : Publish a container's port(s) to the host
-# --detach       : Run container in background and print container ID
-# --init         : Makes process PID=1 be docker-init backed by tini: https://docs.docker.com/engine/reference/run/#specify-an-init-process
+#   --init: Makes process PID=1 be docker-init backed by tini: https://docs.docker.com/engine/reference/run/#specify-an-init-process
 docker run --publish ${hostPort}:${containerPort} \
            --init \
            --name ${containerName} \
            --interactive \
            --tty \
            --detach \
+           --ipc=host \
            ${imageName}:${versionTag}
--- docker_environment/run_dev.sh
+++ docker_environment/run_dev.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # This script runs the docker environment in DEVELOPMENT mode
-#
 # It accepts a positional argument with a route to be mounted
-#
-# Ex:
-#
-# sh run_dev.sh ~/Documents/GitHub/ai/Notebooks
+
+# Example:
+#   sh run_dev.sh ~/Documents/GitHub/ai/Notebooks
 
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 set -e
@@ -18,14 +16,12 @@ else
       additionalVolume="--volume $1:/home/development/Notebooks"
       echo "Additional volume: " $additionalVolume
 fi
-#
+
 # We import values from the configuration file.
 source ./config.sh
 # https://docs.docker.com/config/containers/container-networking/
-# For more info, run : docker run --help
-# --publish list : Publish a container's port(s) to the host
-# --detach       : Run container in background and print container ID
-# --init         : Makes process PID=1 be docker-init backed by tini: https://docs.docker.com/engine/reference/run/#specify-an-init-process
+# For more info, run: docker run --help
+#   --init: Makes process PID=1 be docker-init backed by tini: https://docs.docker.com/engine/reference/run/#specify-an-init-process
 parentDir="$(dirname "$(pwd -P)")"
 docker run --publish ${hostPort}:${containerPort} \
            --init \
@@ -35,5 +31,6 @@ docker run --publish ${hostPort}:${containerPort} \
            --interactive \
            --tty \
            --detach \
+           --ipc=host \
            ${imageName}:${versionTag}
 docker exec ${containerName} bash -c "cd development/lib; python3 setup.py develop"
