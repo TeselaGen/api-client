eb0052e3682cd90d14c8c2f43dee54ec965d3121
Diego Francisco Valenzuela Iturra <diegovalenzuelaiturra@gmail.com>
2021-11-24 05:21:22 +0000

    fixed `.gitignore` file to avoid ignoring `__init__` files

---
+++ teselagen/api/__init__.py
@@ -0,0 +1,4 @@
+from .client import TeselaGenClient  # noqa: F401 # pylint: disable=unused-import
+from .design_client import DESIGNClient  # noqa: F401 # pylint: disable=unused-import
+from .discover_client import DISCOVERClient  # noqa: F401 # pylint: disable=unused-import
+from .test_client import TESTClient  # noqa: F401 # pylint: disable=unused-import
