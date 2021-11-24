5c81de63ab72cf6ddba110460e137f8b7990153a
Diego Francisco Valenzuela Iturra <diegovalenzuelaiturra@gmail.com>
2021-11-24 15:55:02 -0300

    revert file status

--- teselagen/utils/test_utils.py
+++ teselagen/utils/test_utils.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-import pytest
 import time
-from teselagen.utils.utils import wait_for_status
+
+import pytest
 from tenacity import RetryError
 
+from teselagen.utils.utils import wait_for_status
 
-class TestUtils():
+
+class TestUtils:
+
     def test_wait_for_status(self):
-        """Tests wait_for_status happy and sad (timeout) paths with a dummy request"""
+        """Tests wait_for_status happy and sad (timeout) paths with a dummy request."""
         fixed_wait_time = 0.2
         counter_limit = 5
-        validation_method = lambda x: x=="OK"
-        
+
+        def validation_method(x):
+            return x == "OK"
+
         def a_status_request(count_limit: int):
             time.sleep(0.2)
             out = "NOT OK"
@@ -24,23 +29,20 @@ class TestUtils():
         # Happy path
         counter = 0
         result = wait_for_status(
-            method=a_status_request, 
-            validate=validation_method, 
-            fixed_wait_time=fixed_wait_time, 
-            timeout=5, # timeout >= (fixed_wait_time + request_time) * count_limit = (0.2 + 0.2) * 5 = 2.0
+            method=a_status_request,
+            validate=validation_method,
+            fixed_wait_time=fixed_wait_time,
+            timeout=5,  # timeout >= (fixed_wait_time + request_time) * count_limit = (0.2 + 0.2) * 5 = 2.0
             count_limit=counter_limit)
         assert result == "OK"
 
         # Timeout fail (timeout argument is reduced)
-        counter=0
-        with pytest.raises(RetryError):# checks exception is raised
+        counter = 0
+        with pytest.raises(RetryError):  # checks exception is raised
             result = wait_for_status(
-                method=a_status_request, 
-                validate=validation_method, 
-                fixed_wait_time=fixed_wait_time, 
-                timeout=1, 
-                count_limit=counter_limit)
-    
-    
-
-        
+                method=a_status_request,
+                validate=validation_method,
+                fixed_wait_time=fixed_wait_time,
+                timeout=1,
+                count_limit=counter_limit,
+            )
